9caae8cbc1db67f22faaaab0640b38f1e8f038d2
Deepak Sirone Jegan <dsirone@cs.wisc.edu>
2022-05-07 12:04:57 -0500

    Add get_random_word

--- syscall.h
+++ syscall.h
@@ -16,6 +16,7 @@
 #define RUNTIME_SYSCALL_ATTEST_ENCLAVE      1003
 #define RUNTIME_SYSCALL_GET_SEALING_KEY     1004
 #define RUNTIME_SYSCALL_GET_RANDOM          1005
+#define RUNTIME_SYSCALL_GET_RANDOM_WORD     1006
 #define RUNTIME_SYSCALL_EXIT                1101
 
 void handle_syscall(struct encl_ctx* ctx);
